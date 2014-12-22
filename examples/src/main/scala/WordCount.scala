22d02f09ece4ce560cad0a479152b5280017b652
Piotr Szul <piotr.szul@csiro.au>
2014-12-22 14:49:31 +1100

    Fixed examples

--- examples/src/main/scala/WordCount.scala
+++ examples/src/main/scala/WordCount.scala
@@ -1,16 +1,17 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 
 object WordCount { 
     def main(args: Array[String]) { 
-        val driverUrl = args(0)
-        val inputPath = args(1)
-        val sc = new SparkContext(driverUrl, "WordCount")
+        val inputPath = args(0)
+        val sc = new SparkContext(new SparkConf())
         val file = sc.textFile(inputPath) 
         val result = file.flatMap(_.split(" ")) 
         .map(word => (word, 1)) 
         .reduceByKey(_ + _) 
         .toArray()
         println("Result: " + result.toList) 
+        sc.stop()
  } 
 } 
