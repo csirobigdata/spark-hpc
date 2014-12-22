50eb8ae62dcdfc89599ea91c1132932e8e878ab2
Piotr Szul <piotr.szul@csiro.au>
2014-12-22 15:31:23 +1100

    Added resolution of local paths in application arguments

--- tests/submit-wordcount.sh
+++ tests/submit-wordcount.sh
@@ -6,5 +6,5 @@ rm -f WordCount.oe
 
 ../bin/sparkhpc-submit --class WordCount \
  --conf spark.logConf=true \
- -v  core/target/tests-core_2.10-1.0.jar `pwd`/data/lady_of_shalott.txt
+ -v  core/target/tests-core_2.10-1.0.jar ./data/lady_of_shalott.txt
 
