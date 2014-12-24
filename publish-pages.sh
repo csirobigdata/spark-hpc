4e538721e3eb14ae8fad66fb408f25291e4796e2
Piotr Szul <piotr.szul@csiro.au>
2014-12-24 12:14:37 +1100

    Fixed pages updates

--- publish-pages.sh
+++ publish-pages.sh
@@ -3,6 +3,6 @@ git merge master
 pandoc --from markdown --to html --standalone README.md -o README.html
 git add .
 git commit -m "Update html pages"
-git push
+git push --all
 git checkout master
 
